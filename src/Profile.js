904ea3c61e9b03c797aaa9137058608282adaeee
shubham-cloudsufi <shubham.semwal@cloudsufi.com>
2022-05-17 14:28:54 +0530

    svg and css changes

--- src/Profile.js
+++ src/Profile.js
@@ -4,21 +4,8 @@ import { useLocation } from "react-router-dom";
 const ProfileWrapperTile = styled("div")`
   .data {
     background-color: #d9c7c7;
-    /* position: absolute;
-    top: 10%;
-    align-items: center;
-    text-align: center;
-    width: 100%; */
-    height: 32rem;
+    height: 36rem;
   }
-
-  /* .image_profile{
-    position: absolute;
-    border-radius: 50%;
-    z-index: 1;
-    top: 3rem;
-    right: 39rem;
-  } */
 
   .profile_header {
     background-color: white;
@@ -38,95 +25,34 @@ const ProfileWrapperTile = styled("div")`
   }
 
   .all_data {
-    /* position: absolute; */
-    /* background-color: #d9c7c7; */
-    width: 100%;
-    top: 2rem;
     display: flex;
-    /* flex-wrap: wrap; */
-    height: 52%;
-    margin: 0px 60px;
-    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: left;
+    /* background-color: aquamarine; */
+    margin-top: 3rem;
   }
 
   .data_tag {
-    padding: 4rem 0 0 0;
-    /* width: 100%; */
+    /* background-color: blue; */
+    width: 50rem;
   }
 
-  /* .data_entry {
-    display: flex;
-    justify-content: center;
-    margin: 2rem;
-  } */
   .data_name {
-    background-color: #d9c7c7;
+    /* background-color: green; */
     font-weight: 100;
-    display: grid;
-    padding: 1rem 0 0 0;
-    text-align: left;
-    align-items: center;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    margin: 0 10%;
+    padding: 1rem;
   }
   .data_user {
-    /* position: absolute;
-    width: 100%;
-    text-align: center; */
-    font-weight: 100;
-    /* width: 100%; */
-    margin: 0% 0 0 2%;
   }
   @media screen and (max-width: 1000px) {
-    /* .data {
-    background-color: #d9c7c7;
-    margin: 12% 20%;
-    height: 26rem;
-    width: 50rem;
-    display: flex;
-    flex-flow: wrap;
-    } */
-
+  
     .image_profile {
       right: 20rem;
     }
-    /* .data_entry {
-      margin: 14px;
-    } */
-    /* .data_name {
-      background-color: #d9c7c7;
-      font-weight: 100;
-      margin: 10px 0 0 50px;
-    } */
-    /* .data_user {
-      font-weight: 100;
-    }
-    h1 {
-      font-size: 1.5rem;
-    } */
+  
   }
   @media screen and (max-width: 500px) {
-    /* .data {
-      margin: 19% 20%;
-      width: 74%;
-      height: 62%;
-    }
-    .data_entry {
-      margin: 14px;
-    } */
-    /* .data_name {
-      background-color: #d9c7c7;
-      font-weight: 100;
-      margin: 10px 0 0 50px;
-    } */
-    /* .data_user {
-      font-weight: 100;
-    }
-    
-    h1 {
-      font-size: 1.5rem;
-    } */
     .image_profile {
       right: 8rem;
     }
